371a91971b3c3ff94ead7ab7338e4662d0bb2d71
Ramazan <romka1501@gmail.com>
2022-09-30 23:18:20 +0500

    applying react properties

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployersList from "../employers-list/employers-list";
+import EmployersAddForm from "../employers-add-form/employers-add-form";
+
+import './app.css';
+
+function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Alex M.', salary: 15000, increase: false},
+    ];
+
+    return (
+      <div className="App">
+        <AppInfo />
+
+          <div className="search-panel">
+              <SearchPanel />
+              <AppFilter />
+          </div>
+
+          <EmployersList data={data}/>
+          <EmployersAddForm />
+      </div>
+    );
+}
+
+export default App;
